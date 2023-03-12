497a1283114506e7bf5d06d05cc3e2ccfa1d5b53
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-12 04:07:47 -0400

    Update handletovideostats.py

--- handletovideostats.py
+++ handletovideostats.py
@@ -15,63 +15,27 @@ if not os.path.exists(os.path.join(dir_path, "api_key.txt")):
 with open(os.path.join(dir_path, "api_key.txt")) as f:
     api_key = f.read().strip()
 
-# prompt user for input
-print("Starting at newest, how many videos do you want statistics for?")
-number_of_videos = input()
-
-print("Enter Handle id:")
-handle_id = input()
-
-# get channel ID based on handle ID
-url = f"https://yt.jaybee.digital/api/channels?part=channels&handle={handle_id[1:]}"
-retries = 0
-while True:
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            channel_id = response.json()["items"][0]["id"]
-            if channel_id.startswith("UC"):
-                break
-    except:
-        pass
-    retries += 1
-    if retries >= 5:
-        print("Error: failed to retrieve channel ID.")
-        exit()
-
-# fetch statistics and tags for channel's newest videos
-url = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&channelId={channel_id}&part=snippet,id&order=date&maxResults={number_of_videos}"
-response = requests.get(url)
-if response.status_code == 200:
-    data = response.json()
-    for item in data["items"]:
-        if item["id"]["kind"] == "youtube#video":
-            video_id = item["id"]["videoId"]
-            # fetch statistics
-            url = f"https://www.googleapis.com/youtube/v3/videos?key={api_key}&id={video_id}&part=statistics"
+n = int(input("Starting at newest, how many videos do you want statistics for? "))
+        handle_id = input("Enter Handle id: ")
+        url = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&channelId={handle_id}&part=snippet,id&order=date&maxResults=50"
+        try:
             response = requests.get(url)
-            if response.status_code == 200:
-                statistics = response.json()["items"][0]["statistics"]
-            else:
-                statistics = {}
-
-            # fetch tags
-            url = f"https://www.googleapis.com/youtube/v3/videos?key={api_key}&id={video_id}&part=snippet"
-            response = requests.get(url)
-            if response.status_code == 200:
-                tags = response.json()["items"][0]["snippet"]["tags"]
-            else:
-                tags = []
-
-            # save data to file
-            data = {
-                "statistics": statistics,
-                "tags": tags
-            }
-            with open(os.path.join(dir_path, f"{video_id}.json"), "w") as f:
-                json.dump(data, f, indent=4)
-            # open the file in default text editor
-            os.startfile(os.path.join(dir_path, f"{video_id}.json"))
-
-print("Video(s) have been saved to project folder. Enter any key to exit.")
-input()
+            data = json.loads(response.text)
+            video_ids = [item['id']['videoId'] for item in data['items'] if item['id']['kind'] == 'youtube#video']
+            video_data = []
+            for i in range(n):
+                url = f"https://www.googleapis.com/youtube/v3/videos?key={api_key}&id={video_ids[i]}&part=snippet,statistics"
+                response = requests.get(url)
+                data = json.loads(response.text)
+                if 'tags' not in data['items'][0]['snippet']:
+                    print(f"No data found for video with id {video_ids[i]}")
+                    continue
+                video_data.append({
+                    "title": data['items'][0]['snippet']['title'],
+                    "views": int(data['items'][0]['statistics']['viewCount']),
+                    "likes": int(data['items'][0]['statistics']['likeCount']),
+                    "dislikes": int(data['items'][0]['statistics']['dislikeCount']),
+                    "tags": data['items'][0]['snippet']['tags']
+                })
+            if not video_data:
+                print("No data found for the given handle.")
