e75e2ab7cddcff944398b7716c33d7b74fab8d9b
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-12 05:32:01 -0400

    make more terms
    Added support for month, day

--- searchacct.py
+++ searchacct.py
@@ -13,17 +13,32 @@ with open('api_key.txt', 'r') as f:
 
 # Ask for user input
 year = input('What Year? ')
+month = input('What Month? ')
+day = input('What Day? ')
+time = input('What Time? ')
 query = input('What Query? ')
 subs = input('How many Subs? ')
-published_before = input('Published Before? ')
-published_after = input('Published After? ')
 
 url = 'https://www.googleapis.com/youtube/v3/search?part=snippet'
 url += f'&key={api_key}'
 
 if year:
-    url += f'&publishedAfter={year}-01-01T00:00:00Z'
-    url += f'&publishedBefore={year}-12-31T23:59:59Z'
+    if month:
+        if day:
+            if time:
+                date_time = f'{year}-{month}-{day}T{time}:00Z'
+                url += f'&publishedAfter={date_time}'
+                url += f'&publishedBefore={date_time}'
+            else:
+                date = f'{year}-{month}-{day}'
+                url += f'&publishedAfter={date}T00:00:00Z'
+                url += f'&publishedBefore={date}T23:59:59Z'
+        else:
+            url += f'&publishedAfter={year}-{month}-01T00:00:00Z'
+            url += f'&publishedBefore={year}-{month}-31T23:59:59Z'
+    else:
+        url += f'&publishedAfter={year}-01-01T00:00:00Z'
+        url += f'&publishedBefore={year}-12-31T23:59:59Z'
 if query:
     url += f'&q={query}'
 if subs:
