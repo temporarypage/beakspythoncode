afb25c8c3e87ed4cedcb09ca76d46c38e86bb134
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-12 13:05:45 -0400

    Update ver.py

--- ver.py
+++ ver.py
@@ -1,6 +1,7 @@
 import time
 
-print("BeakModule 3/12/2023 6:08 AM EST version")
+print("BeakModule 3/12/2023 1:05 PM EST version")
 print("+Added [5] Search Accts For Claiming Purposes!")
+print("+Added Functionality For Suggestions!"
 time.sleep(1.2)
 # if you are reading this I just like to be cliché
