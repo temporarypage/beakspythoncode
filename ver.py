b67d75fa353643a8860a1035ac54ba7be7e6e873
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-12 04:30:19 -0400

    Update ver.py

--- ver.py
+++ ver.py
@@ -1,6 +1,6 @@
 import time
 
 print("BeakModule 3/12/2023 4:27 AM EST version")
-print("Getting into BeakModule...")
+print("Wait to enter into BeakModule...")
 time.sleep(1.2)
 # if you are reading this I just like to be cliché
