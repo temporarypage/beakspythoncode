407f885a6fcfa39f07bb86dc7f968960f93044f2
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-12 13:23:43 -0400

    Update suggestions.py

--- suggestions.py
+++ suggestions.py
@@ -1,12 +1,12 @@
 suggestion = input("Enter your suggestion: ")
         timestamp = datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S %Z%z')
         message = {
-        "content": f"New suggestion: {suggestion}\nTimestamp: {timestamp}",
-        "username": "Suggestion Bot",
-        "avatar_url": "https://i.imgur.com/4M34hi2.png"
+            "content": f"New suggestion: {suggestion}\nTimestamp: {timestamp}",
+            "username": "Suggestion Bot",
+            "avatar_url": "https://i.imgur.com/4M34hi2.png"
         }
         headers = {
-        "Content-Type": "application/json"
+            "Content-Type": "application/json"
         }
         webhook_url = "https://discord.com/api/webhooks/1084522191485403267/PZI--rnJKwVyMWxRLkAkGGQbgkdqInnDh81p99XZr0TvSnYd6Cco5I3O30ZmxN99TpEO"
         request = urllib.request.Request(webhook_url, json.dumps(message).encode(), headers=headers)
