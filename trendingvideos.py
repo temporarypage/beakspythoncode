27522155af656a832f182072715e9b38f16bab3d
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-12 03:47:31 -0400

    Create trendingvideos.py

---
+++ trendingvideos.py
@@ -0,0 +1,22 @@
+import urllib.request
+from datetime import datetime
+import pytz
+import json
+import requests
+
+print("Getting 25 trending video titles")
+
+        # fetch top 10 trending videos and their titles
+        url = f"https://www.googleapis.com/youtube/v3/videos?key={api_key}&chart=mostPopular&part=snippet&maxResults=25"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            titles = [item["snippet"]["title"] for item in data["items"]]
+
+            # save data to file
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            with open(os.path.join(dir_path, f"trending{timestamp}.json"), "w") as f:
+                json.dump(titles, f, indent=4)
+
+            print(f"Video titles have been saved to trending{timestamp}.json. Enter any key to exit.")
+            input()
