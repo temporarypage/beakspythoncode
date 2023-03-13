5605013b00f4d6b4f8ef58cb6c583ac21049f691
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-13 00:50:29 -0400

    Update ver.py

--- ver.py
+++ ver.py
@@ -3,5 +3,7 @@ import time
 print("BeakModule 3/13/2023 12:49 AM EST version")
 print("+ Added [5] Search Accts For Claiming Purposes! Fixed!")
 print("+ Not Just for Youtube Now!")
+time.sleep(0.4)
+print("Getting into BeakModule API...")
 time.sleep(1.2)
 # if you are reading this I just like to be cliché
