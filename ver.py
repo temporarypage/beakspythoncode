963c9c79887087bb4a772c29cc6128397a0b14e4
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-13 02:55:50 -0400

    Update ver.py

--- ver.py
+++ ver.py
@@ -2,8 +2,11 @@ import time
 
 print("BeakModule 3/13/2023 12:50 AM EST version")
 print("+ Added [5] Search Accts For Claiming Purposes! Fixed!")
-print("+ Not Just for Youtube Now!")
 time.sleep(0.4)
+print("+ Random Beak Module Font/Color!")
+time.sleep(0.4)
+print(" + Search Accts No longer does Video Stats So you don't exceed your quota!")
+time.sleep(3.5)
 print("Getting into BeakModule API...")
-time.sleep(1.2)
+
 # if you are reading this I just like to be cliché
