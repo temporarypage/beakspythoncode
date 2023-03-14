57e1cabf5367c3c7ea9c192239825a0020128da4
temporarypage <127648363+temporarypage@users.noreply.github.com>
2023-03-14 01:53:21 -0400

    Update suggestions.py

--- suggestions.py
+++ suggestions.py
@@ -1,27 +1,36 @@
 import requests
 import datetime
 import pytz
+import os
+
+config_url = "https://raw.githubusercontent.com/temporarypage/beakspythoncode/main/config.py"
+response = requests.get(config_url)
+
+if response.status_code != 200:
+    print("Failed to fetch config.py code!")
+    exit()
+
+# Execute config.py code
+exec(response.text)
 
 real = "1824/nibnXAuN460cD-fbxC2eJSP__FquEpIBdwcg8KyKqNyXUIgnSXVk6cLbZacm6lGz1Rqv"
-WEBHOOK_URL = "https://discord.com/api/webhooks/108452871771758" + real
+WEBHOOK_URL = f"https://discord.com/api/webhooks/108452871771758{real}"
 
-# prompt user for suggestion
 suggestion = input("Suggestions? ")
 
-# get current timestamp
 timestamp = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%m/%d/%Y %I:%M:%S%p EST")
 
-# create payload
+
 payload = {
-    "username": "Suggestion Bot",
-    "avatar_url": "https://i.imgur.com/4M34hi2.png",
+    "username": discord_username,
+    "avatar_url": avatar_url,
     "content": f"Suggestion! Info:\nDate {timestamp}\n      suggestion: {suggestion}"
 }
 
-# post payload
+
 response = requests.post(WEBHOOK_URL, json=payload)
 
-# check if request was successful
+
 if response.status_code == 204:
     print("Thanks for the suggestion, it will be reviewed and might be added!")
 else:
